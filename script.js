07f3cd5dd60eda5707130adc8f9cc26734f69d59
Askeladdd <94636669+Askeladdd@users.noreply.github.com>
2022-03-09 14:58:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    console.log("Hello world!")//1
+    function Hello1() {  //2
+          console.log("Hello world!"); 
+      }
+      function Hello2() {  //3
+        alert("Hello world!"); 
+    }
